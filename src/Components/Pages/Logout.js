95b7928588da51659d5df0a6aeab38bc28985cb3
adam <adame.barhdadi@student.vinci.be>
2023-10-10 17:37:10 +0200

    commit

--- src/Components/Pages/Logout.js
+++
@@ -1,9 +0,0 @@
-import { clearAuthenticatedUser } from '../../utils/auths';
-import Navigate from '../Router/Navigate';
-
-const Logout = () => {
-  clearAuthenticatedUser();
-  Navigate('/');
-};
-
-export default Logout;
