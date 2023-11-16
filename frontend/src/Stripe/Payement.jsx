63a9bd294778130a01fa9005cbfbdb40a127b1cd
diego <diego.gilson@student.vinci.be>
2023-11-16 09:27:55 +0100

    feature 5.0

--- frontend/src/Stripe/Payement.jsx
+++ frontend/src/Stripe/Payement.jsx
@@ -17,69 +17,81 @@ const Checkout = ({ price }) => {
     loading: true,
   });
 
-  const [clientSecretKlarna, setClientSecretKlarna] = useState({
-    clientToken: "",
-    loading: true,
-  });
+  const [clientSecretKlarna, setClientSecretKlarna] = useState("");
   //call to api to get clientsecret
   const getKlarnaClientSecret = async () => {
-    const data = await fetch("/api/create-klarna-session", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        intent: "buy",
-        purchase_country: "BE",
-        purchase_currency: "EUR",
-        locale: "fr-BE",
-        order_amount: 1,
-        order_tax_amount: 0,
-        order_lines: [
-          {
-            type: "physical",
-            //reference: "",
-            name: "clothes",
-            quantity: 1,
-            unit_price: 1,
-            tax_rate: 0,
-            total_amount: `${price}`,
-            total_discount_amount: 0,
-            total_tax_amount: 0,
-            //image_url: "https://www.exampleobjects.com/logo.png",
-            //product_url: "https://www.estore.com/products/f2a8d7e34",
-          },
-        ],
-      }),
-    });
+    try {
+      const data = await fetch("/api/create-klarna-session", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          intent: "buy",
+          purchase_country: "BE",
+          purchase_currency: "EUR",
+          locale: "fr-BE",
+          order_amount: 1,
+          order_tax_amount: 0,
+          order_lines: [
+            {
+              type: "physical",
+              //reference: "",
+              name: "clothes",
+              quantity: 1,
+              unit_price: 1,
+              tax_rate: 0,
+              total_amount: `${price}`,
+              total_discount_amount: 0,
+              total_tax_amount: 0,
+              //image_url: "https://www.exampleobjects.com/logo.png",
+              //product_url: "https://www.estore.com/products/f2a8d7e34",
+            },
+          ],
+        }),
+      });
+    } catch (error) {
+      console.error(error);
+    }
 
-    const response = await data.json;
+    const response = await data.json();
+    console.log(response);
     const client_token = response.client_token;
 
-    setClientSecretKlarna({
-      client_token: client_token,
-      loading: false,
-    });
+    setClientSecretKlarna(client_token);
   };
 
-  getKlarnaClientSecret();
-  // Initialize Klarna once the script is loaded
-  Klarna.Payments.init({
-    client_token: clientSecretKlarna.clientToken,
-  });
+  // Initialize Klarna Payments
+  //Klarna.Payments.init({ client_token: clientSecretKlarna });
+  // Load Klarna payment methods into container
+  useEffect(() => {
+    Klarna.Payments.init({
+      client_token: clientSecretKlarna.clientToken,
+    });
+    Klarna.Payments.load({
+      container: "#klarna_container",
+    });
+  }, [clientSecretKlarna.clientToken]);
+  useEffect(() => {
+    getKlarnaClientSecret();
+  }, []);
 
+  /*
   useEffect(() => {
     // Load Klarna script
-    /*
+    
     const script = document.createElement("script");
     script.src = "https://x.klarnacdn.net/kp/lib/v1/api.js";
     script.async = true;
     document.body.appendChild(script);
-    */
+    
 
     // Cleanup: Remove the script from the document when the component unmounts
+    
     return () => {
       document.body.removeChild(script);
     };
+    
   }, [clientSecretKlarna.clientToken]);
+  */
 
   const createPaymentIntent = async () => {
     const response = await axios.post("/api/create-payment-intent", {
@@ -91,13 +103,6 @@ const Checkout = ({ price }) => {
       loading: false,
     });
   };
-
-  // Initialize Klarna Payments
-  //Klarna.Payments.init({ client_token: clientSecretKlarna });
-  // Load Klarna payment methods into container
-  Klarna.Payments.load({
-    container: "#klarna_container",
-  });
 
   useEffect(() => {
     createPaymentIntent();
